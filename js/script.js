06b291e570364e60c2a1f48631163202585d2823
GustavoRSL <d7gustavo@yahoo.com.br>
2021-07-22 18:38:14 -0300

    Adicionando funcionalidade de login pelo localstorage

--- js/script.js
+++ js/script.js
@@ -123,10 +123,9 @@ function cadastrar() {
         let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]');
         listaUser.push({
             nomeCad: nome.value,
-            usuarioCad: usuario.value,
+            userCad: usuario.value,
             senhaCad: senha.value
-        }
-        )
+        })
 
         localStorage.setItem('listaUser', JSON.stringify(listaUser));
 
@@ -150,5 +149,43 @@ function cadastrar() {
 // Botão Logar
 
 function logar() {
+    let usuario = document.querySelector('#usuario');
+    let userLabel = document.querySelector('#userLabel');
+    let senha = document.querySelector('#senha');
+    let senhaLabel = document.querySelector('#senhaLabel');
+
+    let msgErro = document.querySelector('#msgErro');
+    let listaUser = [];
+
+    let userValid = {
+        nome: null,
+        user: null,
+        senha: null
+    }
+
+    listaUser = JSON.parse(localStorage.getItem('listaUser'));
+    listaUser.forEach((item) => {
+        if(usuario.value == item.userCad && senha.value == item.senhaCad) {
+            userValid = {
+                nome: item.nomeCad,
+                user: item.userCad,
+                senha: item.senhaCad
+            }
+        }
+    })
     
+    if(usuario.value == userValid.user && senha.value == userValid.senha) {
+        // Direcionar a página de compras
+        window.location.href = '../register.html';
+    }
+    else {
+        // Msg Erro
+        userLabel.setAttribute('style', 'color: red');
+        usuario.setAttribute('style', 'border-color: red');
+        senhaLabel.setAttribute('style', 'color: red');
+        senha.setAttribute('style', 'border-color: red');
+        msgErro.setAttribute('style', 'display: block');
+        msgErro.innerHTML = 'Usuário ou senha incorretos';
+        usuario.focus()
+    }
 }
